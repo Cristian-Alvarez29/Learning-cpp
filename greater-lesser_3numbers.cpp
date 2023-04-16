e5eff68a476ce2485c38bf8b3e4da82f26107ccf
Cristian-Alvarez29 <84606386+Cristian-Alvarez29@users.noreply.github.com>
2023-04-15 20:55:28 -0600

    Update greater-lesser_3numbers.cpp

--- greater-lesser_3numbers.cpp
+++ greater-lesser_3numbers.cpp
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 /*run this program using the console pauser or add your own getch, system("pause") or input loop*/
 int main() {
-	
-	int major{},minor{},num1{},num2{},num3{};
+
+int major{},minor{},num1{},num2{},num3{};
     
     std::cout<<"Enter the first number: ";
     std::cin>>num1;
